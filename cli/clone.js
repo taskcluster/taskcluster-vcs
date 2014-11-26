cb81d60661d6e3a657ffe7395482501977d97646
James Lal <james@lightsofapollo.com>
2014-11-25 20:18:43 -0800

    Add initial clone caching support

--- cli/clone.js
+++ cli/clone.js
@@ -1,5 +1,105 @@
+'use strict';
+
 var ArgumentParser = require('argparse').ArgumentParser;
+var Promise = require('promise');
 var detect = require('../vcs/detect_remote');
+var run = require('../vcs/run');
+var render = require('json-templater/string');
+var request = require('superagent-promise');
+var mkdirp = Promise.denodeify(require('mkdirp'));
+var fs = require('mz/fs');
+var fsPath = require('path');
+var urlJoin = require('url-join');
+var URL = require('url');
+
+/**
+Determines if the clone has a cache if it does return a url do it.
+*/
+function hasCloneCache(config, url) {
+  // normalize the url to the "name"
+  var components = URL.parse(url);
+  var cacheName = render(
+    config.cloneCache.cacheName,
+    { name: urlJoin(components.host, components.pathname) }
+  )
+
+  var cacheUrl = render(config.cloneCache.baseUrl, {
+    cacheName: cacheName
+  });
+
+  return request.head(cacheUrl).end().then(function(res) {
+    if (res.ok) {
+      var cacheDir = render(config.cloneCache.cacheDir, {
+        env: process.env
+      });
+
+      return {
+        url: cacheUrl,
+        dest: fsPath.join(cacheDir, cacheName)
+      };
+    }
+    return null;
+  });
+}
+
+function useClone(config, source, dest) {
+  return detect(source)
+    .then(function(vcsConfig) {
+      var module = require('../vcs/' + vcsConfig.type);
+      var clone = new module.Clone(config);
+      return clone.run(source, dest);
+    })
+}
+
+function useCache(config, cache, dest) {
+  // For efficiency we keep a cache of all downloaded dependencies the structure
+  // used closely mirrors to what we use on the server.
+  return fs.exists(dest).
+    then(function(hasDest) {
+      // Mimic the behaviours of git/hg and do not allow users to clobber
+      // themselves accidentally.
+      if (hasDest) {
+        throw new Error('Cannot extract to existing directory ' + dest);
+      }
+      return mkdirp(fsPath.dirname(cache.dest))
+    })
+    .then(function() {
+      return fs.exists(cache.dest)
+    })
+    .then(function(hasCacheDest) {
+      // We are lazy and do not re-download anything that has been downloaded at
+      // any point in the past.
+      if (hasCacheDest) {
+        // Extract the already downloaded cache...
+        return cacheExtract(config, cache.dest, dest);
+      }
+      return downloadAndExtractCache(config, cache, dest);
+    })
+}
+
+function downloadAndExtractCache(config, cache, dest) {
+  var cmd = render(config.cloneCache.get, {
+    url: cache.url,
+    dest: cache.dest
+  });
+
+  return run('/bin/bash', ['-c', cmd]).
+    then(function() {
+      return cacheExtract(config, cache.dest, dest);
+    });
+}
+
+function cacheExtract(config, source, dest) {
+  var cmd = render(config.cloneCache.extract, {
+    source: source,
+    dest: dest
+  });
+
+  return mkdirp(dest)
+    .then(function() {
+      return run('/bin/bash', ['-c', cmd]);
+    });
+}
 
 module.exports = function main(config, argv) {
   var parser = new ArgumentParser({
@@ -18,12 +118,12 @@ module.exports = function main(config, argv) {
   });
 
   var args = parser.parseArgs(argv);
-  detect(args.url)
-    .then(function(vcsConfig) {
-      var url = vcsConfig.url;
-      var module = require('../vcs/' + vcsConfig.type);
-      var clone = new module.Clone(config);
-      return clone.run(args.url, args.dest);
+  return hasCloneCache(config, args.url)
+    .then(function(cache) {
+      if (cache) {
+        return useCache(config, cache, args.dest);
+      }
+      return useClone(config, args.url, args.dest);
     })
     .catch(function(e) {
       process.nextTick(function() {
--- vcs/run.js
+++ vcs/run.js
@@ -8,9 +8,15 @@ Wrapper around process spawning with extra logging.
 */
 function run(bin, args, opts) {
   var proc = spawn(bin, args, assign({ stdio: 'inherit' }, opts));
+  var start = Date.now();
+  var cmd = [bin].concat(args).join(' ');
+  console.log('[tc-vcs] run start : %s', cmd);
   return new Promise(function(accept, reject) {
     proc.on('error', reject);
     proc.once('exit', function(code) {
+      console.log(
+        '[tc-vcs] run end : %s (%d) in %s ms', cmd, code, Date.now() - start
+      );
       if (code === 0) accept();
       reject(new Error(util.format(
         'Running "%s %s" has failed %d',
