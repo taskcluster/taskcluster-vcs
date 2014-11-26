cd5783995064f4dbd31b2e5068667fa774a7243d
James Lal <james@lightsofapollo.com>
2014-11-25 23:47:32 -0800

    checkout revision

--- vcs/git.js
+++ vcs/git.js
@@ -1,6 +1,13 @@
+var assign = require('object-assign');
 var run = require('./run');
 var exec = require('mz/child_process').exec;
 var createCommand = require('./command');
+
+function runGit(git, cwd, commands, opts) {
+  opts = assign({ cwd: cwd }, opts);
+  opts.env = assign({}, process.env, opts.env);
+  return run(git, commands, opts);
+}
 
 module.exports.Clone = createCommand(function(source, dest) {
   return run(this.config.git, ['clone', source, dest]);
@@ -15,3 +22,28 @@ module.exports.Revision = createCommand(function(source) {
     return results[0].trim();
   });
 })
+
+module.exports.CheckoutRevision = createCommand(function(
+  path,
+  repository,
+  ref,
+  revision
+) {
+  return runGit(this.config.git, path, [
+    'fetch',
+    repository,
+    ref
+  ])
+  .then(function() {
+    return runGit(this.config.git, path, [
+      'reset',
+      '--hard'
+    ]);
+  }.bind(this))
+  .then(function() {
+    return runGit(this.config.git, path, [
+      'checkout',
+      revision
+    ]);
+  }.bind(this))
+});
--- vcs/hg.js
+++ vcs/hg.js
@@ -1,6 +1,13 @@
+var assign = require('object-assign');
 var run = require('./run');
 var exec = require('mz/child_process').exec;
 var createCommand = require('./command');
+
+function runHg(hg, cwd, commands, opts) {
+  opts = assign({ cwd: cwd }, opts);
+  opts.env = assign({}, process.env, opts.env);
+  return run(hg, commands, opts);
+}
 
 module.exports.Clone = createCommand(function(source, dest) {
   return run(this.config.hg, ['clone', source, dest]);
@@ -14,4 +21,25 @@ module.exports.Revision = createCommand(function(source) {
   .then(function(results) {
     return results[0].trim();
   });
-})
+});
+
+module.exports.CheckoutRevision = createCommand(function(
+  path,
+  repository,
+  ref,
+  revision
+) {
+  return runHg(this.config.hg, path, [
+    'pull',
+    '-r',
+    revision,
+    repository
+  ])
+  .then(function() {
+    return runHg(this.config.hg, path, [
+      'update',
+      '-C',
+      revision
+    ]);
+  }.bind(this));
+});
