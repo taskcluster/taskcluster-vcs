33c4b9b709a36e64c7fb8f92bea7de67ce4afb9b
James Lal <james@lightsofapollo.com>
2015-03-19 01:52:33 -0700

    update cache script

--- cache.sh
+++ cache.sh
@@ -12,19 +12,33 @@ emulator_url () {
   echo "http://hg.mozilla.org/mozilla-central/raw-file/default/b2g/config/$1/sources.xml"
 }
 
+g_emulator_url() {
+  echo "https://raw.githubusercontent.com/mozilla-b2g/b2g-manifest/master/$1"
+}
+
 run_clone https://github.com/lightsofapollo/build-mozharness
 run_clone https://hg.mozilla.org/build/mozharness
 run_clone https://hg.mozilla.org/build/tools
 run_clone https://hg.mozilla.org/mozilla-central
+run_clone https://hg.mozilla.org/mozilla-central/
+run_clone https://hg.mozilla.org/integration/b2g-inbound/
 run_clone https://hg.mozilla.org/integration/b2g-inbound
 run_clone https://hg.mozilla.org/integration/mozilla-inbound
+run_clone https://hg.mozilla.org/integration/mozilla-inbound/
 run_clone https://hg.mozilla.org/integration/gaia
 run_clone https://hg.mozilla.org/integration/gaia-central
+run_clone http://hg.mozilla.org/integration/fx-team/
+run_clone http://hg.mozilla.org/integration/fx-team
+
+
+run_clone https://hg.mozilla.org/integration/gaia-central
+run_clone https://hg.mozilla.org/integration/gaia-central/
 
 run_clone https://git.mozilla.org/b2g/B2G
 run_clone https://github.com/mozilla-b2g/gaia
 run_clone https://github.com/mozilla-b2g/B2G
 run_clone https://github.com/mozilla-b2g/B2G-manifest
+run_clone https://github.com/mozilla-b2g/moztt
 
 run_clone https://github.com/mozilla/gecko-dev
 
@@ -35,6 +49,14 @@ run_repo $(emulator_url emulator-kk)
 run_repo $(emulator_url flame)
 run_repo $(emulator_url flame-kk)
 run_repo $(emulator_url nexus-4)
+run_repo $(emulator_url emulator-l)
+run_repo $(emulator_url dolphin)
+run_repo $(emulator_url dolphin-512)
+
+run_repo $(g_emulator_url emulator.xml)
+run_repo $(g_emulator_url emulator-jb.xml)
+run_repo $(g_emulator_url emulator-kk.xml)
+run_repo $(g_emulator_url emulator-l.xml)
 
 echo 'Running all tasks in parallel you may see log spam now...'
 wait
