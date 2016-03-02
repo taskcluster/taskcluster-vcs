490ee5e9e83c95fc32f6a4be093f36022394d4a5
Greg Arndt <arndt.greg@gmail.com>
2016-03-02 12:37:27 -0600

    Bump version to 2.3.28

--- src/cli/moz-cache.js
+++ src/cli/moz-cache.js
@@ -154,7 +154,7 @@ function generateCloneTaskDefinition(repo) {
       deadline: deadline,
       scopes: ['queue:create-artifact:*', 'index:insert-task:tc-vcs.v1.clones.*'],
       payload: {
-        image: 'taskcluster/taskcluster-vcs:2.3.26',
+        image: 'taskcluster/taskcluster-vcs:2.3.28',
         command: params,
         maxRunTime: 3600,
         features: {
@@ -203,7 +203,7 @@ function generateRepoCacheTaskDefinition(emulator, type) {
       scopes: ['queue:create-artifact:*', 'index:insert-task:tc-vcs.v1.repo-project.*'],
       routes: [`index.tc-vcs.v1.repo-project.${indexHash}`],
       payload: {
-        image: 'taskcluster/taskcluster-vcs:2.3.26',
+        image: 'taskcluster/taskcluster-vcs:2.3.28',
         command: params,
         maxRunTime: 3600,
         features: {
